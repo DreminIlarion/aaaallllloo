dbd396e871c0ea338804d17eafaeb78336b041be
DreminIlarion <sonic.dremin@gmail.com>
2025-01-27 17:54:29 +0500

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,331 @@
+import React, { useState, useEffect } from 'react';
+import Cookies from 'js-cookie'; // Для работы с куками
+import axios from 'axios';
+const Form = () => {
+  const [formData, setFormData] = useState({
+    top_n: '',
+    gender: '',
+    age: '',
+    sport: '',
+    foreign: '',
+    gpa: '',
+    points: 0,
+    bonus_points: '',
+    exams: [],
+    reception_form: '',
+    priority: '',
+    education: '',
+    study_form: ''
+  });
+
+  const [responseMessage, setResponseMessage] = useState('');
+  const [recommendations, setRecommendations] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+
+
+  // Хэндлер для изменений в форме
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevData => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleExamClick = (exam) => {
+    setFormData(prevFormData => {
+      const updatedExams = prevFormData.exams.includes(exam)
+        ? prevFormData.exams.filter(item => item !== exam)
+        : [...prevFormData.exams, exam];
+      return { ...prevFormData, exams: updatedExams };
+    });
+  };
+
+
+  
+  
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Получаем куки
+    const accessToken = Cookies.get('access');
+    const refreshToken = Cookies.get('refresh');
+    console.log('ЭТО КУКИ КОТОРЫЕ Я ПОЛУЧАЮ ПЕРЕД ОТПРАВКОЙ:', {
+        accessToken,
+        refreshToken
+    });
+
+    // Логируем отправляемые данные
+    console.log('Отправляемые данные:', {
+        ...formData,
+        access_token: accessToken,
+        refresh_token: refreshToken,
+    });
+
+    try {
+        // Логируем перед отправкой
+        console.log('Делаем запрос с параметром credentials: "include"');
+        
+        const response = await fetch('https://personal-account-fastapi.onrender.com/predict/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',  // Убедитесь, что куки передаются
+            body: JSON.stringify({
+                ...formData,
+                access_token: accessToken,
+                refresh_token: refreshToken,
+            })
+        });
+
+        console.log('Ответ от сервера:', response);
+        // Проверяем статус ответа
+        if (response.ok) {
+            const data = await response.json();
+            console.log('Ответ с сервера (JSON):', data);
+
+            if (data.status === 'ok') {
+                console.log('Рекомендации получены:', data.data);
+                setRecommendations(data.data);
+                setIsModalOpen(true);
+            } else {
+                console.log('Ошибка при обработке данных от сервера.');
+                setResponseMessage('Ошибка при обработке данных.');
+            }
+        } else {
+            console.log('Ошибка при отправке данных, статус:', response.status);
+            setResponseMessage('Ошибка при отправке данных.');
+        }
+    } catch (error) {
+        console.error('Ошибка отправки данных:', error);
+        setResponseMessage('Произошла ошибка при отправке данных.');
+    }
+};
+
+  
+  
+
+  return (
+    <div className="container mx-auto p-6">
+      <form onSubmit={handleSubmit} className="bg-white p-8 shadow-xl rounded-lg w-full max-w-xl ml-7">
+        {/* Поле для количества направлений */}
+        <label className="block mb-4 text-sm font-semibold">
+          Количество направлений:
+          <input
+            type="number"
+            value={formData.top_n}
+            onChange={handleChange}
+            name="top_n"
+            className="w-full p-2 mt-2 border rounded-lg focus:ring-2 focus:ring-purple-500"
+            required
+          />
+        </label>
+
+        {/* Поле для выбора пола */}
+        <fieldset className="space-y-4 mb-6">
+          <label className="block text-sm font-semibold">
+            Пол:
+            <select
+              value={formData.gender}
+              name="gender"
+              onChange={handleChange}
+              className="w-full p-2 mt-2 border rounded-lg focus:ring-2 focus:ring-purple-500"
+              required
+            >
+              <option value="">Выберите пол</option>
+              <option value="М">Мужской</option>
+              <option value="Ж">Женский</option>
+            </select>
+          </label>
+
+          {/* Поле для возраста */}
+          <label className="block text-sm font-semibold">
+            Возраст:
+            <input
+              type="number"
+              value={formData.age}
+              name="age"
+              onChange={handleChange}
+              className="w-full p-2 mt-2 border rounded-lg focus:ring-2 focus:ring-purple-500"
+              required
+            />
+          </label>
+
+          {/* Поле для вида спорта */}
+          <label className="block text-sm font-semibold">
+            Вид спорта:
+            <input
+              type="text"
+              value={formData.sport}
+              name="sport"
+              onChange={handleChange}
+              className="w-full p-2 mt-2 border rounded-lg focus:ring-2 focus:ring-purple-500"
+              required
+            />
+          </label>
+
+          {/* Поле для среднего балла GPA */}
+          <label className="block text-sm font-semibold">
+            Средний балл (GPA):
+            <input
+              type="number"
+              step="0.01"
+              value={formData.gpa}
+              name="gpa"
+              onChange={handleChange}
+              className="w-full p-2 mt-2 border rounded-lg focus:ring-2 focus:ring-purple-500"
+              required
+            />
+          </label>
+
+          {/* Поле для общего количества баллов */}
+          <label className="block text-sm font-semibold">
+            Общее количество баллов:
+            <input
+              type="range"
+              min="0"
+              max="310"
+              step="1"
+              value={formData.points}
+              onChange={(e) => setFormData({ ...formData, points: e.target.value })}
+              className="w-full mt-2"
+            />
+            <span className="text-sm">{formData.points} баллов</span>
+          </label>
+
+          {/* Поле для дополнительных баллов */}
+          <label className="block text-sm font-semibold">
+            Дополнительные баллы:
+            <input
+              type="number"
+              value={formData.bonus_points}
+              name="bonus_points"
+              onChange={handleChange}
+              className="w-full p-2 mt-2 border rounded-lg focus:ring-2 focus:ring-purple-500"
+              required
+            />
+          </label>
+
+          {/* Поле для выбора экзаменов */}
+          <label className="block text-sm font-semibold">
+            Выберите экзамены:
+            <div className="grid grid-cols-2 gap-2 mt-2">
+              {['Русский язык', 'Математика', 'Физика', 'Химия', 'Биология', 'Информатика', 'История', 'Обществознание', 'Литература', 'География', 'Иностранный язык'].map((exam) => (
+                <div
+                  key={exam}
+                  className={`p-2 border rounded-lg cursor-pointer ${ formData.exams.includes(exam) ? 'bg-purple-500 text-white' : 'bg-gray-200' }`}
+                  onClick={() => handleExamClick(exam)}
+                >
+                  {exam}
+                </div>
+              ))}
+            </div>
+          </label>
+
+          {/* Поле для приоритета */}
+          <label className="block text-sm font-semibold">
+            Приоритет:
+            <input
+              type="number"
+              value={formData.priority}
+              name="priority"
+              onChange={handleChange}
+              className="w-full p-2 mt-2 border rounded-lg focus:ring-2 focus:ring-purple-500"
+              required
+            />
+          </label>
+
+          {/* Поле для выбора формы приема */}
+          <label className="block text-sm font-semibold">
+            Вид приема:
+            <select
+              value={formData.reception_form}
+              name="reception_form"
+              onChange={handleChange}
+              className="w-full p-2 mt-2 border rounded-lg focus:ring-2 focus:ring-purple-500"
+              required
+            >
+              <option value="">Выберите вид приема</option>
+              <option value="Общий конкурс">Общий конкурс</option>
+              <option value="По договору">По договору</option>
+            </select>
+          </label>
+
+          {/* Поле для выбора вида образования */}
+          <label className="block text-sm font-semibold">
+            Вид образования:
+            <select
+              value={formData.education}
+              name="education"
+              onChange={handleChange}
+              className="w-full p-2 mt-2 border rounded-lg focus:ring-2 focus:ring-purple-500"
+              required
+            >
+              <option value="">Выберите вид образования</option>
+              <option value="Начальное общее образование">Начальное общее образование</option>
+              <option value="Среднее общее образование">Среднее общее образование</option>
+              <option value="Высшее образование">Высшее образование</option>
+            </select>
+          </label>
+
+          {/* Поле для выбора формы обучения */}
+          <label className="block text-sm font-semibold">
+            Форма обучения:
+            <select
+              value={formData.study_form}
+              name="study_form"
+              onChange={handleChange}
+              className="w-full p-2 mt-2 border rounded-lg focus:ring-2 focus:ring-purple-500"
+              required
+            >
+              <option value="">Выберите форму обучения</option>
+              <option value="Очная">Очная</option>
+              <option value="Заочная">Заочная</option>
+            </select>
+          </label>
+
+          {/* Кнопка для отправки формы */}
+          <button
+            type="submit"
+            className="mt-4 bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 focus:ring-2 focus:ring-blue-400"
+          >
+            Рассчитать рекомендации
+          </button>
+        </fieldset>
+      </form>
+      
+      {/* Модальное окно с результатами */}
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center">
+          <div className="bg-white p-6 rounded-lg w-1/2">
+            <h2 className="text-xl font-semibold mb-4">Рекомендации</h2>
+            <ul>
+              {recommendations.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+            <button
+              onClick={() => setIsModalOpen(false)}
+              className="mt-4 bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 focus:ring-2 focus:ring-red-400"
+            >
+              Закрыть
+            </button>
+          </div>
+        </div>
+      )}
+
+      {/* Сообщение об ошибке */}
+      {responseMessage && (
+        <div className="mt-4 text-red-600">
+          <p>{responseMessage}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Form;
---
+++ src/components/Profile.js
@@ -0,0 +1,226 @@
+import React, { useState, useEffect } from 'react';
+import { useUser } from '../context/UserContext';
+import { Link } from 'react-router-dom';
+import { FaUserCircle } from 'react-icons/fa';
+import Form from './Form';
+import ClassifierForm from './MiniClassifier';
+import Chat from './Chat';
+
+const Profile = () => {
+  const { user, logout, updateUser } = useUser();
+  const [activeSection, setActiveSection] = useState(null); // Хранит активный раздел
+  const [profileData, setProfileData] = useState(user || {});
+  const [isChatVisible, setIsChatVisible] = useState(false);
+  
+  const toggleChatVisibility = () => {
+    setIsChatVisible(!isChatVisible);
+  };
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('profileData'));
+    if (storedData) {
+      setProfileData(storedData);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('profileData', JSON.stringify(profileData));
+  }, [profileData]);
+
+  const saveChanges = () => {
+    updateUser(profileData);
+    alert('Профиль успешно обновлён!');
+  };
+
+  return (
+    <div className="flex flex-col font-sans">
+      {/* Header */}
+      <header
+        className="w-full bg-blue-800 text-white shadow-md fixed top-0 z-50"
+        style={{
+          background: 'linear-gradient(90deg, rgba(255,255,255,1) 0%, rgb(25, 29, 255) 0%, rgba(0,99,255,1) 100%)',
+        }}
+      >
+        <div className="container mx-auto px-6 py-4 flex justify-between items-center">
+          <h1 className="text-2xl font-bold">Личный кабинет</h1>
+          <div className="flex space-x-4">
+            <Link to="/help" className="hover:underline">
+              Помощь
+            </Link>
+            <Link to="/contact" className="hover:underline">
+              Контакты
+            </Link>
+          </div>
+        </div>
+      </header>
+
+      <div className="flex flex-grow mt-16">
+        {/* Sidebar Navigation */}
+        <div
+          className="w-64 h-screen fixed left-0 top-0 text-white shadow-lg flex flex-col pt-16"
+          style={{
+            background: 'linear-gradient(180deg, rgba(255,255,255,1) 0%, rgb(25, 29, 255) 0%, rgba(0,99,255,1) 100%)',
+          }}
+        >
+          <div className="flex items-center px-6 py-4 border-b border-blue-700">
+            <FaUserCircle className="text-3xl text-white mr-3" />
+            {user ? (
+              <span className="text-xl font-semibold">{user.email}</span>
+            ) : (
+              <Link to="/login" className="text-white hover:underline">
+                Войти
+              </Link>
+            )}
+          </div>
+          <ul className="mt-4 flex-grow">
+            {user && (
+              <li className="mb-2">
+                <button
+                  onClick={() => setActiveSection('myData')}
+                  className={`w-full text-left px-6 py-3 focus:outline-none ${
+                    activeSection === 'myData' ? 'text-white' : 'text-gray-200 hover:text-white'
+                  } transition-colors duration-200`}
+                >
+                  Мои данные
+                </button>
+              </li>
+            )}
+            {user && (
+              <li className="mb-2">
+                <button
+                  onClick={() => setActiveSection('form')}
+                  className={`w-full text-left px-6 py-3 focus:outline-none ${
+                    activeSection === 'form' ? 'text-white' : 'text-gray-200 hover:text-white'
+                  } transition-colors duration-200`}
+                >
+                  Predict
+                </button>
+              </li>
+            )}
+            <li className="mb-2">
+              <button
+                onClick={() => setActiveSection('classifier')}
+                className={`w-full text-left px-6 py-3 focus:outline-none ${
+                  activeSection === 'classifier' ? 'text-white' : 'text-gray-200 hover:text-white'
+                } transition-colors duration-200`}
+              >
+                Free predict
+              </button>
+            </li>
+            {user && (
+              <li className="mb-2">
+                <button
+                  onClick={() => logout()}
+                  className="w-full text-left px-6 py-3 focus:outline-none text-gray-200 hover:text-white transition-colors duration-200"
+                >
+                  Выход
+                </button>
+              </li>
+            )}
+          </ul>
+          <div className="px-6 py-4 text-sm text-center border-t border-blue-600">
+            <p>© 2025 TyuIU</p>
+          </div>
+        </div>
+
+        {/* Main Content */}
+        <div className="flex-1 ml-64 p-8 min-h-screen bg-white relative">
+          {activeSection === 'myData' && (
+            <div className="mb-8">
+              <h2 className="text-3xl font-bold mb-6 text-center text-black">Мои данные</h2>
+              <div className="p-6 bg-white border border-gray-300 rounded-md shadow-md max-w-lg mx-auto">
+                <form className="space-y-4">
+                  <label htmlFor="first_name" className="block text-black">Имя:</label>
+                  <input
+                    type="text"
+                    id="first_name"
+                    name="first_name"
+                    value={profileData.first_name || ''}
+                    onChange={(e) => setProfileData({ ...profileData, first_name: e.target.value })}
+                    className="w-full p-2 rounded-md bg-gray-100 text-black border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                  />
+                  <label htmlFor="last_name" className="block text-black">Фамилия:</label>
+                  <input
+                    type="text"
+                    id="last_name"
+                    name="last_name"
+                    value={profileData.last_name || ''}
+                    onChange={(e) => setProfileData({ ...profileData, last_name: e.target.value })}
+                    className="w-full p-2 rounded-md bg-gray-100 text-black border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                  />
+                  <label htmlFor="dad_name" className="block text-black">Отчество:</label>
+                  <input
+                    type="text"
+                    id="dad_name"
+                    name="dad_name"
+                    value={profileData.dad_name || ''}
+                    onChange={(e) => setProfileData({ ...profileData, dad_name: e.target.value })}
+                    className="w-full p-2 rounded-md bg-gray-100 text-black border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                  />
+                  <label htmlFor="bio" className="block text-black">О себе:</label>
+                  <textarea
+                    id="bio"
+                    name="bio"
+                    rows="4"
+                    value={profileData.bio || ''}
+                    onChange={(e) => setProfileData({ ...profileData, bio: e.target.value })}
+                    className="w-full p-2 rounded-md bg-gray-100 text-black border border-gray-300 resize-none focus:outline-none focus:ring-2 focus:ring-blue-400"
+                  ></textarea>
+                </form>
+                <button
+                  className="block w-full py-3 mt-6 text-white bg-blue-500 rounded-md hover:bg-blue-600"
+                  onClick={saveChanges}
+                >
+                  Сохранить изменения
+                </button>
+              </div>
+            </div>
+          )}
+
+          {activeSection === 'form' && (
+            <div className="mb-8">
+              <h2 className="text-3xl font-bold mb-6 text-center text-black">Predict</h2>
+              <Form />
+            </div>
+          )}
+
+          {activeSection === 'classifier' && (
+            <div className="mb-8">
+              <h2 className="text-3xl font-bold mb-6 text-center text-black">Free predict</h2>
+              <ClassifierForm />
+            </div>
+          )}
+
+          {!activeSection && (
+            <div className="text-black text-center">
+              <h2 className="text-3xl font-bold mb-6">Добро пожаловать!</h2>
+              <p>Выберите пункт из меню слева.</p>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="fixed bottom-4 right-4">
+        {!isChatVisible ? (
+          <button
+            onClick={toggleChatVisibility}
+            className="bg-blue-500 text-white rounded-full p-4 shadow-lg hover:bg-blue-600 transition duration-200"
+          >
+            Чат
+          </button>
+        ) : (
+          <div className="bg-white shadow-lg rounded-lg w-280 h-196 p-4 relative">
+            <button
+              onClick={toggleChatVisibility}
+              className="absolute top-0 right-1 text-gray-500 hover:text-gray-700"
+            >
+              ✕
+            </button>
+            <Chat />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
